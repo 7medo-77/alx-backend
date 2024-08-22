0f6e2e1f3290303bcd3b49c0ab5183ebe3b61e83
hmedo <hmedo@7medo>
2024-08-22 03:21:44 +0300

    basic_cache.py_03-21

---
+++ 0x01-caching/basic_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+module defining class BasicCache
+"""
+
+
+BaseCaching = __import__("base_cahing").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        """
+        init method which is inherited from parent
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        overriding put method from parent
+        """
+        if not key or not item:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        overriding get method from parent
+        """
+        if not key or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
