548db6b445fa4fa17d78a2222e3ca504d6877090
hmedo <hmedo@7medo>
2024-08-22 05:22:10 +0300

    __pycache___05-22

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,54 @@
+
+#!/usr/bin/env python3
+"""
+Module defining class BasicCache
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Class inheriting from BaseCahing
+    """
+
+    def __init__(self):
+        """
+        init method which is inherited from parent
+        """
+        super().__init__()
+        self.fifo_array = []
+
+    def put(self, key, item):
+        """
+        overriding put method from parent
+        """
+        if not key or not item:
+            pass
+
+        if len(list(self.cache_data.keys())) < 4:
+            if key in self.cache_data:
+                del self.cache_data[key]
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+        elif len(list(self.cache_data.keys())) == 4:
+            if key in self.cache_data:
+                del self.cache_data[key]
+                self.cache_data[key] = item
+            else:
+                discard_key = list(self.cache_data.keys())[-1]
+                print('DISCARD: {}'.format(discard_key))
+                self.cache_data.pop(discard_key)
+                self.cache_data[key] = item
+                assert len(list(self.cache_data.keys())) == 4, "somethingwrong"
+
+    def get(self, key):
+        """
+        overriding get method from parent
+        """
+        if not key or key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
